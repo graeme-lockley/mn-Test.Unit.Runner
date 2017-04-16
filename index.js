415f94d2318a5292aa1f5a4f9d3b91875a2dbbb7
Graeme Lockley <graemel@no9.co.za>
2017-04-16 17:31:16 +0200

    refactor: curried run parameters

--- index.js
+++ index.js
@@ -2,27 +2,28 @@ const FS = require("fs");
 const Path = require("path");
 
 
-function run(base, options) {
-    options = options ? options : {};
-    options.filter = options.filter ? options.filter : filename => true;
+function run(base) {
+    return options => {
+        options.filter = options.filter ? options.filter : filename => true;
 
-    function runTestsInDir(directory) {
-        FS.readdirSync(directory).forEach(file => {
-            const fileName = directory + Path.sep + file;
+        function runTestsInDir(directory) {
+            FS.readdirSync(directory).forEach(file => {
+                const fileName = directory + Path.sep + file;
 
-            FS.stat(fileName, (err, stat) => {
-                if (stat.isFile()) {
-                    if (options.filter && options.filter(fileName)) {
-                        require(fileName);
+                FS.stat(fileName, (err, stat) => {
+                    if (stat.isFile()) {
+                        if (options.filter && options.filter(fileName)) {
+                            require(fileName);
+                        }
+                    } else if (stat.isDirectory()) {
+                        runTestsInDir(fileName);
                     }
-                } else if (stat.isDirectory()) {
-                    runTestsInDir(fileName);
-                }
+                });
             });
-        });
-    }
+        }
 
-    runTestsInDir(base);
+        runTestsInDir(base);
+    };
 }
 
 
